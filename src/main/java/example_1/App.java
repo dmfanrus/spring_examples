abf8b476404cd049339deffcdd52e7ed29d61e3b
dmfanrus <mihail.gurko@gmail.com>
2017-12-28 19:53:00 +0300

    Second commit

--- src/main/java/example_1/App.java
+++ src/main/java/example_1/App.java
@@ -18,10 +18,8 @@ public class App {
 
     public static void main(String[] args) {
 //        App app = new App();
-
 //        app.client = new Client("1", "John Smith");
 //        app.eventLogger = new ConsoleEventLogger();
-//
 //        app.logEvent("Some event for user 1");
     }
 
